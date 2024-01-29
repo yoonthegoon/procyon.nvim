e8dbe68feacdb69ece691f835fbf05c92a04c790
Yunis Yilmaz <yunis@Yuniss-MacBook-Pro.local>
2024-01-28 22:23:11 -0500

    first commit

---
+++ lua/melange/palettes/dark.lua
@@ -0,0 +1,35 @@
+-- stylua: ignore
+return {
+  a = {
+    bg      = "#222e39",
+    float   = "#3f4852",
+    sel     = "#525b64",
+    ui      = "#6d767f",
+    com     = "#8f9398",
+    fg      = "#c9d1db",
+  },
+  b = {
+    red     = "#eb6788",
+    yellow  = "#b88d00",
+    green   = "#4fa700",
+    cyan    = "#00a599",
+    blue    = "#009ce1",
+    magenta = "#d169e3",
+  },
+  c = {
+    red     = "#c94569",
+    yellow  = "#937000",
+    green   = "#3d8500",
+    cyan    = "#00847a",
+    blue    = "#007cb4",
+    magenta = "#b443c5",
+  },
+  d = {
+    red     = "#aa204e",
+    yellow  = "#715600",
+    green   = "#2e6600",
+    cyan    = "#00655d",
+    blue    = "#00608c",
+    magenta = "#9c08ac",
+  },
+}
