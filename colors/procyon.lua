a989e72faf8d2049d7a99ae2228189e6ef8f5e5b
Yunis Yilmaz <yunis@Yuniss-MacBook-Pro.local>
2024-01-29 03:22:38 -0500

    add thingy

--- colors/procyon.lua
+++ colors/procyon.lua
@@ -1,2 +1,6 @@
 vim.opt.background = "dark"
 vim.g.colors_name = "lush_theme"
+package.loaded["lush_theme.lush_template"] = nil
+
+-- include our theme file and pass it to lush to apply
+require("lush")(require("lush_theme.lush_template"))
